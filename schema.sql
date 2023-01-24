a1bcf2cb19f4cf19cda6c1cfd9d217ba6871bed1
jorgegoco <jorgegoco70@gmail.com>
2023-01-24 11:50:23 +0100

    Add one column and new records

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOL,
-    weight_kg DECIMAL  
+    weight_kg DECIMAL,
+    species VARCHAR(100)  
 );
